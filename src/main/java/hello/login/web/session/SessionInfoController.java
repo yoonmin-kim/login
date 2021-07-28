95e4f6b4f2c090a2651a9ae31d9382691915fdf2
yoonmin-kim <rladbsals23@gmail.com>
2021-07-28 22:55:35 +0900

    docs : 세션 정보와 타임아웃 설정
    일반적으로 사용자들은 로그아웃버튼을 누르지않고 브라우저를 종료한다.
    때문에 서버입장에서는 세션을 메모리에 계속해서 가지고있게 된다.
    
    사용자가 많은 서버라면 메모리부하를 고려해야될 상황이된다.
    
    때문에 기본적으로 사용자가 가장 최근에 서버로 요청한 시간(lastAccessedTime)을
    기준으로 최대유지시간을 계산해서(MaxInactiveInterval) 세션을 삭제시킨다.
    
    유지시간은 설정파일으로 설정 가능하다
    server.servlet.session.timeout=60
    
    (주의)
    세션은 메모리를 사용하기 때문에 세션에 보관하는 데이터는 정말 필요한 데이터만
    딱 맞춰서 보관하는게 좋다. 세션에보관하는 데이터수 * 사용자수 로 계산되어 메모리
    사용량이 급격히 증가하기 때문이다.

---
+++ src/main/java/hello/login/web/session/SessionInfoController.java
@@ -0,0 +1,34 @@
+package hello.login.web.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@RestController
+public class SessionInfoController {
+
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return "세션 없음";
+        }
+
+        //세션 데이터 출력
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name -> log.info("session name={}, value={}", name, session.getAttribute(name)));
+
+        log.info("sessionId={}", session.getId());
+        log.info("getMaxInactiveInterval={}", session.getMaxInactiveInterval());
+        log.info("creationTime={}", session.getCreationTime());
+        log.info("lastAccessedTime={}", session.getLastAccessedTime());
+        log.info("isNew={}", session.isNew());
+
+        return "세션 출력";
+    }
+}
