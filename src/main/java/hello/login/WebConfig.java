20166792e18c2db58d93275ffd3b7c26db69ea67
yoonmin-kim <rladbsals23@gmail.com>
2021-07-29 21:42:03 +0900

    feat : 서블릿 필터 - 요청 로그
    사용자의 모든 요청마다 동일한 기능이 필요할 수 있다.
    대표적으로 인증기능이 있다.
    
    모든 서블릿 메서드에 동일한 코드가 들어가게끔 개발자가 직접 구현한다면
    작업도 번거롭고 실수로 빠지는 부분도 생길것이다.
    
    WAS 컨테이너가 사용자의 요청이 서블릿으로 넘어가기이전에 FILTER를 호출한다.
    
    @ServletComponentScan @WebFilter로 필터등록이 가능하지만 필터순서는 설정할 수 없기 때문에
    @Configuartion빈에 FilterRegistrationBean을 등록하였다.
    
    LogFilter에 chain.doFilter 전후로 uuid를 활용하여 로그를 남기도록 하였다.
    때에 따라서 모든 로직에 추적가능한 uuid로그를 남기고 싶을때가 있다.
    >> logback mdc를 참고하자.

---
+++ src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,20 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
