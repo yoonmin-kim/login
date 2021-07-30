88f81aee9ccfe1330f3942ce8baf1789ee03d202
yoonmin-kim <rladbsals23@gmail.com>
2021-07-30 22:59:44 +0900

    feat : 서블릿 필터 - 인증체크
    사용자가 http://domain/items와 같이 url을 직접쳐서 접근을 시도할때
    인증하는 필터기능을 추가한다.
    
    로그인, 회원가입같은 페이지는 미인증 상태여도 접근이 가능해야 하기 때문에
    whiteList로 지정한다.
    
    사용자가 url을 쳤다가 로그인 페이지로 리다이렉트되고 로그인을 했을때
    입력했던 url로 자동으로 리다이렉트 되도록하였다.
    - 필터에서 리다이렉트할때 사용자가 입력했던 URL을 함께 넘겨준다.
    - 로그인컨트롤러에서는 해당 URL을 @RequestParam으로 받아서 리다이렉트 시킨다.

--- src/main/java/hello/login/web/filter/LogFilter.java
+++ src/main/java/hello/login/web/filter/LogFilter.java
@@ -1,6 +1,7 @@
 package hello.login.web.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -70,7 +71,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -89,6 +90,30 @@ public class LoginController {
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Validated @ModelAttribute LoginForm form , BindingResult bindingResult
+            , @RequestParam(defaultValue = "/") String redirectURL
+            , HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 패스워드가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 반환
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:" + redirectURL;
     }
 
 //    @PostMapping("/logout")
