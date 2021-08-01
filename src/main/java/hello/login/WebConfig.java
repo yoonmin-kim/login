4bae5058c0732f81c7543ce600b2bd3e93db7667
yoonmin-kim <rladbsals23@gmail.com>
2021-08-01 18:23:13 +0900

    feat : 스프링 인터셉터 - 인증 체크
    필터와 비교했을 때 개발자 입작에서 훨씬 사용성이 좋다
    1. 코드가 간결함, 오버라이드 메서드가 default로 선언되어 있어서 필요한것만 사용하면됨
       파라미터가 HttpServletRequest, HttpServletResponse로 들어옴
    2. 경로를 좀 더 세밀하게 지정할 수 있음, addPathPatterns, excludePathPatterns

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/logout", "/error", "/css/**", "/*.ico");
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -30,7 +36,7 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
